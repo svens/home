04b0f8f0f6b3d05bdc0f59f8725a2517f83cfc15
Sven Suursoho <sven@alt.ee>
2018-10-07 11:19:59 +0300

    brew: vim 8.1.450

--- Formula/vim.rb
+++ Formula/vim.rb
@@ -2,8 +2,8 @@ class Vim < Formula
   desc "Vi 'workalike' with many additional features"
   homepage "https://www.vim.org/"
   # vim should only be updated every 50 releases on multiples of 50
-  url "https://github.com/vim/vim/archive/v8.1.0250.tar.gz"
-  sha256 "f35193627469f4dc307e9286b21b2c818faaa2c67f03fb4983b58010f26f7f78"
+  url "https://github.com/vim/vim/archive/v8.1.0450.tar.gz"
+  sha256 "19e30d255f429e1ae6a70d1fe08028e1fd14b257ebac7d4470ed1dbed5142648"
   head "https://github.com/vim/vim.git"
 
   def install
